7348ebc648131fd94084cd124dc5518840c3e1a8
Nicoleta Constantin <constantinnicoleta@hotmail.com>
2024-09-11 12:57:32 +0000

    add secret key

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5184*1^fg)lcsr$sq-wq6#f(-ze@+94lqphn@p%d3j7oqx7c31'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-constantinn-habittracke-jkep6cle8d1.ws-eu116.gitpod.io','.herokuapp.com']
 
