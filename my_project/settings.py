ab4ae9f230f2cdcabd97fc5d8c5a1a2661c60c78
Nicoleta Constantin <constantinnicoleta@hotmail.com>
2024-09-14 21:19:30 +0000

    style 'edit habit' and 'add habit' cards

--- my_project/settings.py
+++ my_project/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-constantinn-habittracke-jkep6cle8d1.ws-eu116.gitpod.io','.herokuapp.com']
 
@@ -133,6 +133,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'habits/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
